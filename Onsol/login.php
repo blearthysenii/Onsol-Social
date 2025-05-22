bfa789864c3e4a88856fad3bbf717d670dd5cc70
blearthysenii <bleart.hyseni@umib.net>
2025-05-22 16:08:05 +0200

    Set login as homepage and added 'Create one' link to register

--- Onsol/login.php
+++ Onsol/login.php
@@ -6,16 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $conn->real_escape_string(trim($_POST['username']));
     $password = $_POST['password'];
 
-    
     $sql = "SELECT * FROM users WHERE username = '$username'";
     $result = $conn->query($sql);
 
     if ($result && $result->num_rows === 1) {
         $user = $result->fetch_assoc();
 
-      
         if (password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['user'] = $user; 
             header("Location: index.php");
+            exit;
         } else {
             echo "Incorrect password!";
         }
@@ -23,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "User not found!";
     }
 } else {
-    echo "Write your username and";
+    echo "Write your username and password to login!";
 }
 ?>
 
@@ -40,6 +41,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="password" name="password" placeholder="Password" required /><br />
         <button type="submit">Login</button>
     </form>
+
+    
+    <p>Don't have an account? <a href="register.php">Create one!</a></p>
 </body>
 </html>
-
